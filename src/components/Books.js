16c26e4c5e82f8aa74b19e3b080dd34b92c02867
Atilla Taha Kördüğüm <atillatahaa@gmail.com>
2021-12-13 14:04:21 +0300

    create skeleton on html

---
+++ src/components/Books.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Books = ()=>{
+    return(
+        <div>
+            <h1>
+                Welcome Books
+            </h1>
+        </div>
+    )
+
+}
+export default Books;
---
+++ src/components/Categories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Categories = ()=>{
+    return(
+        <div>
+            <h1>
+                Welcome Categories
+            </h1>
+        </div>
+    )
+
+}
+export default Categories;
