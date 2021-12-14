b813a14a4ca2744b55ce287a95e46ec5b7cba0ef
Atilla Taha Kördüğüm <atillatahaa@gmail.com>
2021-12-14 17:40:19 +0300

    added remove and added functions

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
-const AddBook = () => (
-  <div className="addbook-content">
-    <h4>ADD NEW BOOK</h4>
-    <div className="add-book-btns">
-      <form>
-        <input type="text" />
-        <input type="text" />
-        <input type="submit" value="ADD BOOK" />
+const AddBook = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
 
-      </form>
+  const submitBookToStore = (e) => {
+    e.preventDefault();
+
+    const newBook = {
+      id: 4, // make sure it's unique
+      title,
+      categorie: 'Action',
+      author: 'example',
+      endRate: '20',
+    };
+
+    dispatch(addBook(newBook));
+    setTitle('');
+  };
+
+  return (
+    <div className="addbook-content">
+      <h4>ADD NEW BOOK</h4>
+      <div className="add-book-btns">
+        <form onSubmit={submitBookToStore}>
+          <input type="text" name="title" value={title} onChange={(e) => (setTitle(e.target.value))} />
+          <input type="text" />
+          <input type="submit" onClick={submitBookToStore} value="ADD BOOK" />
+
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default AddBook;
---
+++ src/redux/books/books.js
@@ -0,0 +1,65 @@
+// books.js
+const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+
+// books.js
+const initialState = [
+  {
+    id: 1,
+    categorie: 'Action',
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    endRate: '64',
+  },
+  {
+    id: 2,
+    categorie: 'Action',
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    endRate: '64',
+  },
+  {
+    id: 3,
+    categorie: 'Action',
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    endRate: '64',
+  },
+];
+
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
+export const RemoveBook = (book) => ({
+  type: REMOVE_BOOK,
+  payload: book,
+});
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      /*
+      return a new state object in which the
+      books array will contain a new book object, passed by action.payload.
+      Remember -  you MUSN'T mutate the state. You have to return a new state object - i.e.:
+      return [ ...state, action.payload];
+      */
+      return [...state, action.payload];
+
+    case REMOVE_BOOK:
+    /*
+    use ES6 filter() method to create a
+    new array, which will not contain the book you
+    want to remove from the store (filter by the id key - i.e.:
+    return state.filter(book => book.id !== id);
+    */
+
+      return state.filter((x) => x.id !== action.payload.bookContent.id);
+    default:
+      return state;
+  }
+};
+
+export default reducer;
